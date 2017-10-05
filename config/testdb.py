205fdd25e004faa19775e7478e4f2aa2bf8a6edd
juanrarodriguez18 <dragondr3w@gmail.com>
2017-10-05 20:00:07 +0200

    Cambio menor en los Tests

--- config/testdb.py
+++ config/testdb.py
@@ -10,9 +10,11 @@ db = DBC()
 
 db.insertEmailServer('Gmail','smtp.gmail.com',465,'SMTP')
 EmailServer = db.searchEmailServer('Gmail','SMTP')
-print('['+EmailServer.getName()+']\nHost: '+EmailServer.getHost()+'\nPort: '+str(EmailServer.getPort()))
+print('['+EmailServer.getName()+']\nHost: '+EmailServer.getHost()+'\nPort: '+str(EmailServer.getPort())+
+      '\nProtocol: '+EmailServer.getProtocol())
 EmailServer.setPort(468)
 db.updateEmailServer(EmailServer)
 EmailServer = db.searchEmailServer('Gmail','SMTP')
-print('['+EmailServer.getName()+']\nHost: '+EmailServer.getHost()+'\nPort: '+str(EmailServer.getPort()))
+print('['+EmailServer.getName()+']\nHost: '+EmailServer.getHost()+'\nPort: '+str(EmailServer.getPort())+
+      '\nProtocol: ' + EmailServer.getProtocol())
 db.removeEmailServer('Gmail','SMTP')
